c244d5651a8b30be757e80dd6b165e5d2f24b306
Aldemylla <aldemylla@gmail.com>
2022-10-21 16:38:38 -0300

    fix: variables.scss shared globally

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router';
 import store from './store';
 
 import './styles/global.scss';
+/*
+  The global style import is not in "vite.config.js" because 
+  the his scope there doesn't include the body;
+
+  Also, the import of variables is not here for lack of compatibility.
+*/
 
 const app = createApp(App);
 
