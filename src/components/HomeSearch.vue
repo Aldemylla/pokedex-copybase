1c3a5684503983ea05ac46477a770b77ed9ada9b
Aldemylla <aldemylla@gmail.com>
2022-10-21 17:59:16 -0300

    feat: HomeSearch styles

--- src/components/HomeSearch.vue
+++ src/components/HomeSearch.vue
@@ -1,11 +1,14 @@
 <template>
-  <section>
-    <p>{{ pokemonsCount }} Pokemons for you to choose your favorite</p>
-    <div>
-      <img src="@/assets/icons/search.svg" alt="Search icon" />
+  <section class="home__search">
+    <h2 class="subtitle">
+      {{ pokemonsCount }} Pokemons for you to choose your favorite
+    </h2>
+    <div class="search-bar">
+      <img src="@/assets/icons/search.svg" alt="Search" />
       <input
         type="search"
         :value="search"
+        placeholder="Search"
         @input="SET_SEARCH($event.target.value)"
         @keyup.enter="routerHandler"
       />
@@ -40,3 +43,85 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home__search {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+
+  .subtitle {
+    font-size: 1.2rem;
+    text-align: center;
+    color: $dark-gray;
+  }
+
+  .search-bar {
+    min-width: 190px;
+    width: 60%;
+    height: auto;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+
+    padding: 4px 8px;
+
+    background: $white;
+
+    border: 1px solid $light-gray;
+    border-radius: 8px;
+
+    img {
+      height: 1.2rem;
+    }
+
+    input {
+      width: 100px;
+
+      font-size: 1.2rem;
+      border: none;
+
+      transition: all 0.2s ease-in-out;
+
+      &::placeholder {
+        text-align: center;
+      }
+
+      &:not(:placeholder-shown) {
+        width: 100%;
+      }
+
+      &:focus {
+        outline: none;
+        width: 100%;
+        text-align: left;
+
+        &::placeholder {
+          opacity: 0;
+        }
+      }
+    }
+  }
+
+  @media only screen and (min-width: 769px) and (max-width: 1279px) {
+    .search-bar {
+      width: 70%;
+    }
+  }
+
+  @media only screen and (min-width: 481px) and (max-width: 768px) {
+    .search-bar {
+      width: 80%;
+    }
+  }
+
+  @media only screen and (max-width: 480px) {
+    .search-bar {
+      width: 95%;
+    }
+  }
+}
+</style>
